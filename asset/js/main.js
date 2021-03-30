b68911434be74aec0665e5b5fad1642aca188739
ACERORAAS <acerocol67@gmail.com>
2021-03-29 22:27:25 -0400

    los pokemones estan entrenando

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,3 +1,55 @@
-let titulo = document.getElementById("titulo");
-titulo.innerText="viva chavez"
-document.title= "La floresta"
+let pokemons = [];
+
+function getPokemons() {
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=10&offset=20")
+    .then((respuesta) => respuesta.json())
+
+    .then((datos) => {
+      pokemons = datos.results;
+      crearPokemones(pokemons);
+    })
+
+    .catch((e) => {
+      console.log(e);
+    });
+}
+
+function getImgPokemon(url = "") {
+  if (url.length > 0) {
+    return fetch(url)
+      .then((respuesta) => respuesta.json())
+      .catch((e) => {
+        console.log(e);
+      });
+  } else {
+    alert("la url esta vacia");
+  }
+}
+
+function crearPokemones(data) {
+  let base = document.getElementById("pokemons");
+  for (let i = 0; i < data.length; i++) {
+    let button = document.createElement("div");
+
+    let nombre = document.createElement("h3");
+    let img = document.createElement("img");
+    button.appendChild(nombre);
+    nombre.innerText = data[i].name;
+    getImgPokemon(data[i].url).then((pokemon) => {
+      img.src = pokemon.sprites.front_default;
+
+      button.addEventListener("click", (e) => {
+        let imagenPrincipal = document.getElementById('imgPokemon');
+        imagenPrincipal.src=pokemon.sprites.front_default;
+      });
+
+      button.appendChild(img);
+    });
+    base.appendChild(button);
+  }
+}
+
+window.addEventListener("load", () => {
+  getPokemons();
+  console.log(pokemons);
+});
